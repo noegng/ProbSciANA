c84f4a631e32d4d194019f1b2c5b35145c13ed41
noegng <noe.guenego@gmail.com>
2025-02-25 12:53:17 +0100

    creation projet

---
+++ Graphe.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pb_Sci_Etape_1
+{
+    internal class Graphe
+    {
+        private Dictionary<int, List<int>> listeAdjacence;
+
+        public Graphe(Dictionary<int, List<int>> adjacence)
+        {
+            listeAdjacence = adjacence;
+        }
+
+        // Parcours en Largeur (BFS)
+        public void ParcoursLargeur(int sommetDepart)
+        {
+            Queue<int> file = new Queue<int>();
+            HashSet<int> visite = new HashSet<int>();
+
+            file.Enqueue(sommetDepart);
+            visite.Add(sommetDepart);
+
+            Console.Write("BFS: ");
+
+            while (file.Count > 0)
+            {
+                int sommet = file.Dequeue();
+                Console.Write(sommet + " ");
+
+                foreach (int voisin in listeAdjacence[sommet])
+                {
+                    if (!visite.Contains(voisin))
+                    {
+                        file.Enqueue(voisin);
+                        visite.Add(voisin);
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+
+        // Parcours en Profondeur (DFS) avec pile
+        public void ParcoursProfondeur(int sommetDepart)
+        {
+            Stack<int> pile = new Stack<int>();
+            HashSet<int> visite = new HashSet<int>();
+
+            pile.Push(sommetDepart);
+
+            Console.Write("DFS: ");
+
+            while (pile.Count > 0)
+            {
+                int sommet = pile.Pop();
+
+                if (!visite.Contains(sommet))
+                {
+                    Console.Write(sommet + " ");
+                    visite.Add(sommet);
+                }
+
+                foreach (int voisin in listeAdjacence[sommet])
+                {
+                    if (!visite.Contains(voisin))
+                    {
+                        pile.Push(voisin);
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Noeud.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pb_Sci_Etape_1
+{
+    internal class Noeud
+    {
+        private (int,int) caca;
+    }
+}
