6d2db3aa2db69aaa40615f4df1631be75bd87097
noegng <noe.guenego@gmail.com>
2025-03-05 10:36:41 +0100

    my sql data

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.IO;
-
+using MySql.Data.MySqlClient;
+using System.Data;
 
 
 namespace ProbSciANA
