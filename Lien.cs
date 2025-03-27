d6994b2315f16cd01a94933fd5db65dd88573ab7
Aurélien <aurelienissy18@gmail.com>
2025-03-27 18:53:28 +0100

    Update Lien.cs

--- Lien.cs
+++ Lien.cs
@@ -27,16 +27,6 @@ namespace ProbSciANA
             get { return noeud2; }
             set { noeud2 = value; }
         }
-        public Noeud Noeud1
-        {
-            get { return noeud1; }
-            set { noeud1 = value; }
-        }
-        public Noeud Noeud2
-        {
-            get { return noeud2; }
-            set { noeud2 = value; }
-        }
         public string toString()
         {
             return $"{noeud2.toString(),2 } {noeud1.toString(),2}";
