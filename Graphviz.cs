76b92f593556a25482d2cfe318eb7f39d2977062
noegng <noe.guenego@gmail.com>
2025-03-31 10:04:17 +0200

    graphe couleurs RAPT + qualitée augmentéee

--- Graphviz.cs
+++ Graphviz.cs
@@ -26,23 +26,24 @@ namespace ProbSciANA
         // Définir les couleurs pour chaque ligne
         return idLigne switch
         {
-        "1" => "gold",
-        "2" => "deepskyblue",
-        "3" => "limegreen",
-        "3bis" => "lightskyblue",
-        "4" => "magenta",
-        "5" => "orange",
-        "6" => "springgreen",
-        "7" => "hotpink",
-        "7bis" => "lightpink",
-        "8" => "cyan",
-        "9" => "yellow",
-        "10" => "darkorange",
-        "11" => "brown",
-        "12" => "chartreuse",
-        "13" => "mediumorchid",
-        "14" => "violet",
-            _ => "black", // Couleur par défaut
+        "1" => "#F2C931", // Bouton d'Or
+        "2" => "#216EB4", // Azur
+        "3" => "#9A9940", // Olive
+        "3bis" => "#89C7D6", // Pervenche
+        "4" => "#BB4D98", // Parme
+        "5" => "#DE5D29", // Orange
+        "6" => "#79BB92", // Menthe
+        "7" => "#DF9AB1", // Rose
+        "7bis" => "#79BB92", // Menthe
+        "8" => "#C5A3CA", // Lilas
+        "9" => "#CDC83F", // Acacia
+        "10" => "#DFB039", // Ocre
+        "11" => "#8E6538", // Marron
+        "12" => "#328E5B", // Sapin
+        "13" => "#89C7D6", // Pervenche
+        "14" => "#67328E", // Iris
+        "A" => "#D35E3C", // Coquelicot
+        _ => "black", // Couleur par défaut
         };
     }
 
@@ -60,6 +61,7 @@ namespace ProbSciANA
         dot.AppendLine("graph G {");
         dot.AppendLine("    layout=neato;"); // Utilise le moteur neato
         dot.AppendLine("    overlap=false;");
+        dot.AppendLine("    graph [dpi=1000];");
 
         // Creation de chaque sommet avec sa position   
         foreach (Station vertex in stations)
@@ -67,7 +69,7 @@ namespace ProbSciANA
             var pos = vertex.Nom;
             string longitude = vertex.Longitude.ToString(CultureInfo.InvariantCulture);
             string latitude = vertex.Latitude.ToString(CultureInfo.InvariantCulture);
-            dot.AppendLine($"    \"{pos}\" [pos=\"{longitude},{latitude}!\", label=\"{pos}\", fontsize=8];");
+            dot.AppendLine($"    \"{pos}\" [pos=\"{longitude},{latitude}!\",shape =\"point\", label=\"{pos}\", fontsize=12];");
         }
 
         foreach (Arete edge in aretes)
