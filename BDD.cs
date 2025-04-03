54a25fb52b55b115aefd9a1a8ec63d7e8afbae4a
WinterBete <148715130+WinterBete@users.noreply.github.com>
2025-04-03 18:03:26 +0200

    Refactor Utilisateur class to include additional properties and update constructor; remove redundant project entry from solution file

--- BDD.cs
+++ BDD.cs
@@ -8,21 +8,33 @@ namespace LivinParis
         private int id_utilisateur;
         private string nom;
         private string prenom;
-        private string email;
         private string adresse;
-        private string role;
+        private string telephone;
+        private string email;
+        private string station;
+        private DateTime date_inscription;
+        private string mdp;
 
-        public Utilisateur(string nom, string prenom, string email, string adresse, string role)
+        public Utilisateur(string nom, string prenom, string adresse, string telephone, string email, string station, string mdp)
         {
+            this.id_utilisateur = Requetes.utilisateurs.Count + 1;
             this.nom = nom;
             this.prenom = prenom;
-            this.email = email;
             this.adresse = adresse;
-            this.role = role;
-            
+            this.telephone = telephone;
+            this.email = email;
+            this.station = station;
+            this.date_inscription = DateTime.Now();
+            this.mdp = mdp;
+
+            Insert(nom, prenom, adresse, telephone, email, station, mdp);
         }
 
         #region GetSet
+        public string Id_utilisateur
+        {
+            get { return id_utilisateur; }
+        }
         public string Nom
         {
             get { return nom; }
@@ -53,6 +65,7 @@ namespace LivinParis
 
     public class Client : Utilisateur
     {
+        private id_utilisateur;
         public Client(string nom, string prenom, string email, string adresse, string role, string telephone, string dateNaissance) : base(nom, prenom, email, adresse, role)
         {
             this.telephone = telephone;
