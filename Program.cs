45580db538bd0273f46a51e3eb2343db373d1211
WinterBete <148715130+WinterBete@users.noreply.github.com>
2025-04-03 12:14:08 +0200

    création classe requetes

--- Program.cs
+++ Program.cs
@@ -427,36 +427,4 @@ namespace ProbSciANA
         }
         #endregion
     }
-
-
-    public static class SqlQueries{
-
-            public static void SqlAddUser(string nom, string prenom, string email, string adresse, string role)
-    {
-        string connectionString = "server=localhost;user=root;password=ton_mot_de_passe;database=livinparis;";
-        using (MySqlConnection connection = new MySqlConnection(connectionString))
-        {
-            connection.Open();
-
-            string query = @"INSERT INTO utilisateurs (Nom, Prenom, Email, Adresse, Role)
-                             VALUES (@Nom, @Prenom, @Email, @Adresse, @Role);";
-
-            using (MySqlCommand command = new MySqlCommand(query, connection))
-            {
-                command.Parameters.AddWithValue("@Nom", nom);
-                command.Parameters.AddWithValue("@Prenom", prenom);
-                command.Parameters.AddWithValue("@Email", email);
-                command.Parameters.AddWithValue("@Adresse", adresse);
-                command.Parameters.AddWithValue("@Role", role);
-                command.ExecuteNonQuery();
-            }
-            connection.Close();
-        }
-    }
-
-
-
-
-
-    }
 }
