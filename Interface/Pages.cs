2fbe53972430e5982d804034bf94e2f6e779e5aa
noegng <noe.guenego@gmail.com>
2025-04-30 10:50:26 +0200

    merge

--- Interface/Pages.cs
+++ Interface/Pages.cs
@@ -1025,4 +1025,71 @@ namespace ProbSciANA.Interface
         }
     }
     #endregion
+
+    #region Test2
+
+    public partial class Test2 : Page
+    {
+        public Test2()
+        {
+            InitializeComponent();
+            Loaded += (s, e) => UpdateNavButtons();
+        }
+        private void UpdateNavButtons()
+        {
+            BtnBack.IsEnabled = NavigationService?.CanGoBack == true;
+            BtnForward.IsEnabled = NavigationService?.CanGoForward == true;
+        }
+        private void BtnMode_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Changer de mode (sombre / clair) à implémenter !");
+        }
+        private void BtnAccueil_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService?.Navigate(new StartView());
+        }
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (NavigationService.CanGoBack)
+                NavigationService.GoBack();
+            UpdateNavButtons();
+        }
+        private void BtnForward_Click(object sender, RoutedEventArgs e)
+        {
+            if (NavigationService.CanGoForward)
+                NavigationService.GoForward();
+            UpdateNavButtons();
+        }
+        private void BtnAffichage_Djikstra(object sender, RoutedEventArgs e)
+        {
+            int tempsTrajet = Program.GrapheMétro.AffichageDijkstra(Program.Stations[1], Program.Stations[100]);
+            MessageBox.Show($"Temps de trajet entre {Program.Stations[1].Valeur.nom} et {Program.Stations[100].Valeur.nom} : {tempsTrajet} minutes.");
+        }
+
+        private void BtnAffichage_BF(object sender, RoutedEventArgs e)
+        {
+            int tempsTrajet = Program.GrapheMétro.AffichageBellmanFord(Program.Stations[1], Program.Stations[100]);
+            MessageBox.Show($"Temps de trajet entre {Program.Stations[1].Valeur.nom} et {Program.Stations[100].Valeur.nom} : {tempsTrajet} minutes.");
+        }
+        private void BtnAffichage_Floyd(object sender, RoutedEventArgs e)
+        {
+            int tempsTrajet = Program.GrapheMétro.AffichageFloydWarshall(Program.Stations[1], Program.Stations[100]);
+            MessageBox.Show($"Temps de trajet entre {Program.Stations[1].Valeur.nom} et {Program.Stations[100].Valeur.nom} : {tempsTrajet} minutes.");
+        }
+        private void BtnCheminOptimal(object sender, RoutedEventArgs e)
+        {
+            List<Noeud<(int id, string nom)>> Liste = new List<Noeud<(int id, string nom)>>();
+            Liste.Add(Program.Stations[1]);
+            Liste.Add(Program.Stations[20]);
+            Liste.Add(Program.Stations[40]);
+            Liste.Add(Program.Stations[60]);
+            Liste.Add(Program.Stations[80]);
+            Liste.Add(Program.Stations[100]);
+            Liste.Add(Program.Stations[120]);
+            var programInstance = new Program();
+            int tempsTrajet = programInstance.CheminOptimal(Program.GrapheMétro, Liste);
+            MessageBox.Show($"Temps de trajet entre {Program.Stations[1].Valeur.nom} et {Program.Stations[80].Valeur.nom} en passant par {Program.Stations[20].Valeur.nom}; {Program.Stations[40].Valeur.nom} ;{Program.Stations[60].Valeur.nom} est de : {tempsTrajet} minutes.");
+        }
+    }
+    #endregion
 }
